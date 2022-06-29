295704537a25aa4a1e0ddd3537a6f58970d1b068
Seiry Yu <4397354+seiry@users.noreply.github.com>
2022-06-29 22:46:47 +0800

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
-  log: [{ level: 'query', emit: 'event' }],
+  log: ['query', 'warn', 'error'],
   errorFormat: 'pretty',
 })
-prisma.$on('query', (e) => {
-  console.log(e)
-})
+
+// const prisma = new PrismaClient({
+//   log: [{ level: 'query', emit: 'event' }],
+//   errorFormat: 'pretty',
+// })
+// prisma.$on('query', (e) => {
+//   console.log(e)
+// })
 export default prisma
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
-export const mainTitle = '板板博客哦！'
-export const TitleSub = ' - 板板'
+export const mainTitle = '强哥无敌哦！'
+export const TitleSub = ' - 强哥勇敢飞 上海永相随'
 export const makeTitle = (title: string | null) => {
   if (!title) {
-    return '一望无尽的空地上 伫立着一个板板'
+    return '一望无尽的空地上 伫立着一个李强'
   }
   return `${title}${TitleSub}`
 }
