ddcf7f0df5816835e27ebbe45fd2e69ffc154092
seiry <4397354+seiry@users.noreply.github.com>
2023-08-10 03:51:46 +0800

    add max

--- src/pages/api/spots.ts
+++ src/pages/api/spots.ts
@@ -1,13 +1,9 @@
+import { spot } from '@/db/schema'
+import { db } from '@/db/turso'
 import { NextApiHandler } from 'next'
-import prisma from '../../lib/prisma'
 
 const handler: NextApiHandler = async (req, res) => {
-  const data = await prisma.spot.findMany({
-    select: {
-      spotid: true,
-      name: true,
-    },
-  })
+  const data = db.select().from(spot)
 
   res.status(200).json(data)
   return
