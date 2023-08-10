c4c507365605c910c055acae94975bca60336554
seiry <4397354+seiry@users.noreply.github.com>
2023-08-10 20:39:17 +0800

    update job

--- src/pages/api/secjob.ts
+++ src/pages/api/secjob.ts
@@ -2,9 +2,13 @@ import { mainJob } from '@/lib/job'
 import { NextApiHandler } from 'next'
 
 const handler: NextApiHandler = async (req, res) => {
+  if (req.query.key !== '19890604') {
+    res.status(404).end()
+    return
+  }
   const re = await mainJob()
 
-  res.status(200).end(re)
+  res.status(200).end(re.rowsAffected.toString())
 }
 
 export default handler
