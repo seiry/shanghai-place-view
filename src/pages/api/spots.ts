78873d515364adeab3fce70d85098df626402c15
Seiry Yu <4397354+seiry@users.noreply.github.com>
2024-01-09 05:06:41 +0800

    Refactor API handler to handle errors and return JSON response

--- src/pages/api/spots.ts
+++ src/pages/api/spots.ts
@@ -1,9 +1,15 @@
 import { spot } from '@/db/schema'
 import { db } from '@/db/turso'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-const handler = async () => {
-  const data = await db.select().from(spot).run()
-  return new Response(JSON.stringify(data.rows))
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const data = await db.select().from(spot).run()
+    return res.status(200).json(data.rows)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ error: 'Internal Server Error' })
+  }
 }
 
 export default handler
