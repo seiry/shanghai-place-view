0eb696ab97182978cf08150e5025b3cd077475d8
Seiry Yu <4397354+seiry@users.noreply.github.com>
2024-01-09 05:03:22 +0800

    Refactor API handler to return maxList rows as JSON response

--- src/pages/api/max.ts
+++ src/pages/api/max.ts
@@ -2,37 +2,41 @@ import { log, spot } from '@/db/schema'
 import { db } from '@/db/turso'
 import { and, desc, eq, notInArray } from 'drizzle-orm'
 import { errorMsg } from '../../lib/error'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-const handler = async () => {
-  const time = await db
-    .select()
-    .from(log)
-    .orderBy(desc(log.time))
-    .limit(1)
-    .run()
-  const lastTime = time.rows?.[0]?.time
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const timeResult = await db
+      .select()
+      .from(log)
+      .orderBy(desc(log.time))
+      .limit(1)
+      .run()
+    const lastTime = timeResult.rows?.[0]?.time as number | undefined
 
-  if (!lastTime) {
-    return new Response(JSON.stringify(errorMsg('no date')), {
-      status: 500,
-    })
+    if (!lastTime) {
+      return res.status(500).json(errorMsg('no date'))
+    }
+
+    const maxList = await db
+      .select()
+      .from(log)
+      .innerJoin(spot, eq(log.spotId, spot.spotId))
+      .where(
+        and(
+          eq(log.time, lastTime),
+          notInArray(log.spotId, [102, 107, 104, 106]),
+        ),
+      )
+      .orderBy(desc(log.num))
+      .limit(5)
+      .run()
+
+    return res.status(200).json(maxList.rows)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json(errorMsg('Internal Server Error'))
   }
-
-  const maxList = await db
-    .select()
-    .from(log)
-    .innerJoin(spot, eq(log.spotId, spot.spotId))
-    .where(
-      and(
-        eq(log.time, lastTime as number),
-        notInArray(log.spotId, [102, 107, 104, 106]),
-      ),
-    )
-    .orderBy(desc(log.num))
-    .limit(5)
-    .run()
-
-  return new Response(JSON.stringify(maxList.rows))
 }
 
 export default handler
