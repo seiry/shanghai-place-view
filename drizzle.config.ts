88e53fa65cc409b16dba715a13aab5235ae8a446
Seiry Yu <4397354+seiry@users.noreply.github.com>
2024-01-09 04:24:18 +0800

    Fix dotenv configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'drizzle-kit'
-import * as dotenv from 'dotenv'
-dotenv.config()
+if (!process.env.DATABASE_URL) {
+  require('dotenv').config()
+}
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
